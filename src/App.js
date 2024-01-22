f5a8d922b47832dfa1c00b15869dc2ee83a889e9
abao929 <alexbao929@gmail.com>
2024-01-22 11:37:27 -0500

    please?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
-import { useState } from "react";
-import bakeryData from "./assets/bakery-data.json";
+import './App.css'
+import { useState } from 'react'
+import bakeryData from './assets/bakery-data.json'
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
-  item.image = process.env.PUBLIC_URL + "/" + item.image;
-});
+  item.image = process.env.PUBLIC_URL + '/' + item.image
+})
 /* ############################################################## */
 
 function App() {
@@ -13,19 +13,23 @@ function App() {
   /* add your cart state code here */
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
+      <h2>does changing this make a difference??</h2>
+      {bakeryData.map(
+        (
+          item,
+          index // TODO: map bakeryData to BakeryItem components
+        ) => (
+          <p>Bakery Item {index}</p> // replace with BakeryItem component
+        )
+      )}
       <div>
         <h2>Cart</h2>
         {/* TODO: render a list of items in the cart */}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
